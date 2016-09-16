8544916dd91822441cc5f0f1322eb3a87d1c7a09
Benjamin Gudehus <hastebrot@gmail.com>
2016-09-16 09:19:30 +0200

    (cleanup) Restructure code into sections.

--- src/hastebot.ts
+++ src/hastebot.ts
@@ -1,17 +1,39 @@
+//-------------------------------------------------------------------------------------------------
+// IMPORTS.
+//-------------------------------------------------------------------------------------------------
+
 let path = require("path")
 let _ = require("lodash")
 let Q = require("q")
 let Client = require("hangupsjs")
 
+//-------------------------------------------------------------------------------------------------
+// CLIENT.
+//-------------------------------------------------------------------------------------------------
+
 let client = new Client({
     cookiespath: path.resolve(__dirname, "../data/cookies.json"),
     rtokenpath: path.resolve(__dirname, "../data/refreshtoken.txt")
 })
+
 //client.loglevel("debug")
+//client.logout()
+
 let credentials = {
     auth: Client.authStdin
 }
-let enabledConversations = []
+
+client.connect(() => credentials).then(() => {
+    console.log("client connected")
+    client.getselfinfo().then(event => { 
+        let self_entity = event.self_entity.id.chat_id
+        console.log("self_entity:", self_entity) 
+    })
+}).done()
+
+//-------------------------------------------------------------------------------------------------
+// MESSAGES.
+//-------------------------------------------------------------------------------------------------
 
 let buildMessage = (text) => {
     let builder = new Client.MessageBuilder()
@@ -21,40 +43,48 @@ let buildMessage = (text) => {
     return segments
 }
 
+let botConversations = []
+
+let quotes = require(path.resolve(__dirname, "../data/quotes")).quotes || []
+
 client.on("chat_message", event => {
+    let self_user_id = event.self_event_state.user_id.chat_id
     let conversation_id = event.conversation_id.id
     let sender_id = event.sender_id.chat_id
-    let user_id = event.self_event_state.user_id.chat_id
     let message_segments = event.chat_message.message_content.segment
-
-    console.log("conversation_id:", conversation_id)
-    console.log("sender_id:", sender_id)
-    console.log("user_id:", user_id)
-    console.log("message segments", message_segments)
-
+    
+    let isAdmin = sender_id === self_user_id
+    let isDebug = false
+    let isEnabled = _.includes(botConversations, conversation_id)
+    
     let message = _.chain(message_segments)
         .filter(it => it.type === "TEXT")
         .map(it => it.text)
         .reduce((message, text) => message + text, "")
         .value()
-    console.log(message)
+    console.log(sender_id, message)
+    
+    // hastebotDebug.ts
+    if (isDebug) {
+        console.log("self_user_id:", self_user_id)
+        console.log("conversation_id:", conversation_id)
+        console.log("sender_id:", sender_id)
+        console.log("message segments", message_segments)
+    }
 
-    let isAdmin = sender_id === user_id
-    let isEnabled = _.includes(enabledConversations, conversation_id)
-
+    // hastebotConfig.ts
     if (isAdmin) {
         if (_.startsWith(message, "!bot on")) {
-            enabledConversations = _.uniq(_.concat(enabledConversations, conversation_id))
+            botConversations = _.uniq(_.concat(botConversations, conversation_id))
             client.sendchatmessage(conversation_id, buildMessage("is now enabled"))
         }
         else if (_.startsWith(message, "!bot off")) {
-            enabledConversations = _.uniq(_.without(enabledConversations, conversation_id))
+            botConversations = _.uniq(_.without(botConversations, conversation_id))
             client.sendchatmessage(conversation_id, buildMessage("is now disabled"))
         }
     }
     
-    let quotes = [
-    ]
+    // hastebotQuotes.ts
     if (isEnabled) {
         if (_.startsWith(_.toLower(message), "!quote count")) {
             client.sendchatmessage(conversation_id, buildMessage("has " + quotes.length + " quotes available (0 new quotes pending)"))
@@ -65,12 +95,3 @@ client.on("chat_message", event => {
         }
     }
 })
-
-//client.logout()
-client.connect(() => credentials).then(() => {
-    console.log("client connected")
-    client.getselfinfo().then(event => { 
-        let self_entity = event.self_entity.id.chat_id
-        console.log("self_entity:", self_entity) 
-    })
-}).done()
